e625b1d3e2f544773a65ac9bdd5d6705d1bb0f92
Danylo Duma <147776776+DanyloDuma@users.noreply.github.com>
2025-01-17 23:11:58 +0000

    formatado

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # projeto_m5_20242025
+
 Projeto que avalia o módulo M5 de JavaScript
--- js/tema.js
+++ js/tema.js
@@ -1,20 +1,20 @@
 // Selecionar o interruptor
-const themeToggle = document.getElementById('dark-mode-toggle');
+const themeToggle = document.getElementById("dark-mode-toggle");
 
 // Verificar e aplicar o tema salvo
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme === 'dark') {
-  document.body.classList.add('dark-theme');
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
+  document.body.classList.add("dark-theme");
   themeToggle.checked = true;
 }
 
 // Alterar tema ao clicar no interruptor
-themeToggle.addEventListener('change', () => {
+themeToggle.addEventListener("change", () => {
   if (themeToggle.checked) {
-    document.body.classList.add('dark-theme');
-    localStorage.setItem('theme', 'dark'); // Salvar preferência
+    document.body.classList.add("dark-theme");
+    localStorage.setItem("theme", "dark"); // Salvar preferência
   } else {
-    document.body.classList.remove('dark-theme');
-    localStorage.setItem('theme', 'light'); // Salvar preferência
+    document.body.classList.remove("dark-theme");
+    localStorage.setItem("theme", "light"); // Salvar preferência
   }
 });
